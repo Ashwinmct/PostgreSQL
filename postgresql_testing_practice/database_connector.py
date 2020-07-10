e6a3da2686a7e7397be06d6cdd61e3377ae84196
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-07-10 16:00:26 +0530

    [ASHWIN B]Added. feature to add record to the table

--- postgresql_testing_practice/database_connector.py
+++ postgresql_testing_practice/database_connector.py
@@ -4,13 +4,11 @@ import psycopg2
 connection_object = psycopg2.connect(database="test", user="postgres", password="123", host="127.0.0.1", port="5432")
 print("Opened the database successfully")
 
-#to check table creation
+#to add records to table
 cursor_object = connection_object.cursor()
-cursor_object.execute('''CREATE TABLE COMPANY
-						(ID INT PRIMARY KEY  NOT NULL,
-						NAME TEXT  NOT NULL,
-						AGE INT  NOT NULL,
-						SALARY REAL);''')
+cursor_object.execute("INSERT INTO COMPANY (ID,NAME,AGE,SALARY) VALUES (1, 'Ashwin B' , 23 , 50000.00);")
+cursor_object.execute("INSERT INTO COMPANY (ID,NAME,AGE,SALARY) VALUES (2, 'Vicky G' , 28 , 90000.00);")
+cursor_object.execute("INSERT INTO COMPANY (ID,NAME,AGE,SALARY) VALUES (3, 'Loki O' , 23 , 30000.00);")
 connection_object.commit()
 connection_object.close()
-print("Table Created Successfully")
+print("Table records created Successfully")
