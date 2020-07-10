e0f1b72d011c38733528e497343b5aa0c93161d8
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-07-10 16:43:51 +0530

    [ASHWIN B]Refactored. DELETE a field from the database

--- postgresql_testing_practice/database_connector.py
+++ postgresql_testing_practice/database_connector.py
@@ -7,19 +7,19 @@ def print_table_details(cursor_object, message):
 	for row in table_rows:
 		print("ID=", row[0])
 		print("NAME=", row[1])
-		print("SALARY=", row[2])
+		print("SALARY=", row[2], '\n')
 
 #to check data base opening
 connection_object = psycopg2.connect(database="test", user="postgres", password="123", host="127.0.0.1", port="5432")
 print("Opened the database successfully")
 
 cursor_object = connection_object.cursor()
-print_table_details(cursor_object, "table details before changing")
+print_table_details(cursor_object, "table details before DELETION")
 
 #to UPDATE table records
-cursor_object.execute("UPDATE COMPANY set SALARY = 70000 WHERE ID = 3;")
+cursor_object.execute("DELETE FROM COMPANY WHERE ID = 2;")
 connection_object.commit()
 
-print_table_details(cursor_object, "table details after changing")
+print_table_details(cursor_object, "table details after DELETION")
 connection_object.close()
 print("Table records updated and displayed Successfully")
