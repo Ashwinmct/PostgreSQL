2eb7ff47e6297af8d4414adedd33cf416263effe
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-07-10 16:36:31 +0530

    [ASHWIN B]Refactored. data base table using UPDATE command

--- postgresql_testing_practice/database_connector.py
+++ postgresql_testing_practice/database_connector.py
@@ -1,17 +1,25 @@
 import psycopg2
 
+def print_table_details(cursor_object, message):
+	print(message)
+	cursor_object.execute("SELECT id, name, salary FROM COMPANY;")
+	table_rows = cursor_object.fetchall()
+	for row in table_rows:
+		print("ID=", row[0])
+		print("NAME=", row[1])
+		print("SALARY=", row[2])
+
 #to check data base opening
 connection_object = psycopg2.connect(database="test", user="postgres", password="123", host="127.0.0.1", port="5432")
 print("Opened the database successfully")
 
-#to display table records
 cursor_object = connection_object.cursor()
-cursor_object.execute("SELECT id, name, salary FROM COMPANY;")
-table_rows = cursor_object.fetchall()
-for row in table_rows:
-	print("ID=",row[0])
-	print("NAME=", row[1])
-	print("SALARY=", row[2])
+print_table_details(cursor_object, "table details before changing")
+
+#to UPDATE table records
+cursor_object.execute("UPDATE COMPANY set SALARY = 70000 WHERE ID = 3;")
 connection_object.commit()
+
+print_table_details(cursor_object, "table details after changing")
 connection_object.close()
-print("Table records displayed Successfully")
+print("Table records updated and displayed Successfully")
