5fc5f710168c7993a4a4e1d5b3fb9a581c812558
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-07-10 15:43:01 +0530

    [ASHWIN B]Added. feature to create table in the database

--- postgresql_testing_practice/database_connector.py
+++ postgresql_testing_practice/database_connector.py
@@ -1,4 +1,16 @@
 import psycopg2
 
-conn = psycopg2.connect(database="test", user="postgres", password="123", host="127.0.0.1", port="5432")
+#to check data base opening
+connection_object = psycopg2.connect(database="test", user="postgres", password="123", host="127.0.0.1", port="5432")
 print("Opened the database successfully")
+
+#to check table creation
+cursor_object = connection_object.cursor()
+cursor_object.execute('''CREATE TABLE COMPANY
+						(ID INT PRIMARY KEY  NOT NULL,
+						NAME TEXT  NOT NULL,
+						AGE INT  NOT NULL,
+						SALARY REAL);''')
+connection_object.commit()
+connection_object.close()
+print("Table Created Successfully")
