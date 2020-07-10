3aaedc588617169ea6a5f8a829631a1fc23abf1e
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-07-10 16:14:12 +0530

    [ASHWIN B]Added. feature to display details from the table

--- postgresql_testing_practice/database_connector.py
+++ postgresql_testing_practice/database_connector.py
@@ -4,11 +4,14 @@ import psycopg2
 connection_object = psycopg2.connect(database="test", user="postgres", password="123", host="127.0.0.1", port="5432")
 print("Opened the database successfully")
 
-#to add records to table
+#to display table records
 cursor_object = connection_object.cursor()
-cursor_object.execute("INSERT INTO COMPANY (ID,NAME,AGE,SALARY) VALUES (1, 'Ashwin B' , 23 , 50000.00);")
-cursor_object.execute("INSERT INTO COMPANY (ID,NAME,AGE,SALARY) VALUES (2, 'Vicky G' , 28 , 90000.00);")
-cursor_object.execute("INSERT INTO COMPANY (ID,NAME,AGE,SALARY) VALUES (3, 'Loki O' , 23 , 30000.00);")
+cursor_object.execute("SELECT id, name, salary FROM COMPANY;")
+table_rows = cursor_object.fetchall()
+for row in table_rows:
+	print("ID=",row[0])
+	print("NAME=", row[1])
+	print("SALARY=", row[2])
 connection_object.commit()
 connection_object.close()
-print("Table records created Successfully")
+print("Table records displayed Successfully")
