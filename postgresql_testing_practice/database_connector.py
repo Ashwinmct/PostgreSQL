b54d7b19fc94ea4cca620e3b85845679605be847
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-07-12 14:19:29 +0530

    [ASHWIN B]Refactored. print table details method

--- postgresql_testing_practice/database_connector.py
+++ postgresql_testing_practice/database_connector.py
@@ -4,10 +4,11 @@ def print_table_details(cursor_object, message):
 	print(message)
 	cursor_object.execute("SELECT id, name, salary FROM COMPANY;")
 	table_rows = cursor_object.fetchall()
+	print("\nID\tNAME\tSALARY")
 	for row in table_rows:
-		print("ID=", row[0])
-		print("NAME=", row[1])
-		print("SALARY=", row[2], '\n')
+		print("\n{0}\t{1}\t{2}".format(row[0], row[1], row[2]))
+
+
 
 #to check data base opening
 connection_object = psycopg2.connect(database="test", user="postgres", password="123", host="127.0.0.1", port="5432")
