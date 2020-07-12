7d02cf2877f7e1e265cda1fd6e9de7d9def90400
Ashwinmct <ashwin.b.ashwin00@gmail.com>
2020-07-12 22:48:44 +0530

    [ASHWIN B]Added. feature to export data to a file

--- postgresql_testing_practice/import_and_export.py
+++ postgresql_testing_practice/import_and_export.py
@@ -15,4 +15,8 @@ print("\nID\tNAME\tEMPLOYEE")
 for row in table_rows:
 	print("\n{0}\t{1}\t{2}".format(row[0], row[1], row[2]))
 
+output_file_data = "COPY ({0}) TO STDOUT WITH CSV HEADER".format("SELECT * FROM employee")
+with open('C:\\Users\VIGNESH\\PycharmProjects\\PostgreSQL\\postgresql_testing_practice\\resourse\\workers.csv', 'w') as output_file:
+	cursor_object.copy_expert(output_file_data, output_file)
+
 
